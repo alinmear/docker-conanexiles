4108bb0d8d93c48e30996e20bd7503ac7f531359
Flow86 <656249+Flow86@users.noreply.github.com>
2021-01-05 09:39:56 +0100

    update wine mono and gecko

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -298,9 +298,9 @@ echo -n "Using WINE: "
 wine --version
 
 mkdir -p /usr/share/wine/mono /usr/share/wine/gecko
-test -f /usr/share/wine/mono/wine-mono-5.0.0-x86.msi || wget -q http://dl.winehq.org/wine/wine-mono/5.0.0/wine-mono-5.0.0-x86.msi -O /usr/share/wine/mono/wine-mono-5.0.0-x86.msi
-test -f /usr/share/wine/gecko/wine-gecko-2.47.1-x86_64.msi || wget -q http://dl.winehq.org/wine/wine-gecko/2.47.1/wine-gecko-2.47.1-x86_64.msi -O /usr/share/wine/gecko/wine-gecko-2.47.1-x86_64.msi
-test -f /usr/share/wine/gecko/wine-gecko-2.47.1-x86.msi || wget -q http://dl.winehq.org/wine/wine-gecko/2.47.1/wine-gecko-2.47.1-x86.msi -O /usr/share/wine/gecko/wine-gecko-2.47.1-x86.msi
+test -f /usr/share/wine/mono/wine-mono-5.1.1-x86.msi || wget -q http://dl.winehq.org/wine/wine-mono/5.1.1/wine-mono-5.1.1-x86.msi -O /usr/share/wine/mono/wine-mono-5.1.1-x86.msi
+test -f /usr/share/wine/gecko/wine-gecko-2.47.2-x86_64.msi || wget -q http://dl.winehq.org/wine/wine-gecko/2.47.2/wine-gecko-2.47.2-x86_64.msi -O /usr/share/wine/gecko/wine-gecko-2.47.2-x86_64.msi
+test -f /usr/share/wine/gecko/wine-gecko-2.47.2-x86.msi || wget -q http://dl.winehq.org/wine/wine-gecko/2.47.2/wine-gecko-2.47.2-x86.msi -O /usr/share/wine/gecko/wine-gecko-2.47.2-x86.msi
 
 wget http://crt.usertrust.com/USERTrustRSAAddTrustCA.crt -O /usr/local/share/ca-certificates/USERTrustRSAAddTrustCA.crt
 update-ca-certificates
