f854f12fdb29c73fdb550c5b3f795bdcade1c5b6
alinmear <alinmear@users.noreply.github.com>
2021-04-01 11:21:57 +0200

    Updated log message format

--- src/var/lib/conanexiles/notifier.sh
+++ src/var/lib/conanexiles/notifier.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 notifier_info() {
-    echo ">> INFO: $1"
+    echo "`date +%F %T` INFO $1"
 }
 
 notifier_debug() {
-    echo ">> DEBUG: $1"
+    echo "`date +%F %T` DEBUG $1"
 }
 
 notifier_error() {
-    echo ">> ERROR: $1"
+    echo "`date +%F %T` ERROR $1"
 }
 
 notifier_warn() {
-    echo ">> WARN: $1"
+    echo "`date +%F %T` WARN $1"
 }
