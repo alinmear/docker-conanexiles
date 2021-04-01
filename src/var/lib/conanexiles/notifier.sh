3fec701d1f1cf4e6ceaecfa0cdde82da79f37bc8
alinmear <alinmear@users.noreply.github.com>
2021-04-01 12:46:31 +0200

    Fixed problem with date and update check

--- src/var/lib/conanexiles/notifier.sh
+++ src/var/lib/conanexiles/notifier.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 notifier_info() {
-    echo "`date +%F %T` INFO $1"
+    echo "`date "+%F %T"` INFO $1"
 }
 
 notifier_debug() {
-    echo "`date +%F %T` DEBUG $1"
+    echo "`date "+%F %T"` DEBUG $1"
 }
 
 notifier_error() {
-    echo "`date +%F %T` ERROR $1"
+    echo "`date "+%F %T"` ERROR $1"
 }
 
 notifier_warn() {
-    echo "`date +%F %T` WARN $1"
+    echo "`date "+%F %T"` WARN $1"
 }
